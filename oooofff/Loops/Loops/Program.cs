12c6aead3cb2534a1731a05fb2fdec729876a5a9
GunelYusuf <91114321+GunelYusuf@users.noreply.github.com>
2021-10-03 23:22:34 +0400

    Loops Tasks

---
+++ oooofff/Loops/Loops/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // n ededinin sade yoxsa murekkeb olmasini yoxlayin:
+
+            //Console.Write("ededi daxil edin: ");
+            //int n = int.Parse(Console.ReadLine());
+
+            //bool sadedir = true;
+            //if (n == 1)
+            //{
+            //    Console.WriteLine("ne sadedir ne de murekkeb");
+            //}
+            //else
+            //{
+            //    for (int i = 2; i < n; i++)
+            //    {
+            //        if (n % i == 0)
+            //        {
+            //            sadedir = false;
+
+            //        }
+            //    }
+            //    if (sadedir)
+            //    {
+            //        Console.WriteLine("Sadedir");
+
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Murekkebdir");
+            //    }
+            //}
+
+
+
+
+            //n ededinin nece mertebeli oldugunu tapin:
+
+            //Console.Write("n: ");
+            //int n = int.Parse(Console.ReadLine());
+            //int count = 0;
+
+            //while (n >= 1)
+            //{
+            //    n /= 10;
+            //    count++;
+
+            //}
+            //Console.WriteLine(count);
+
+
+
+            //Console.Write("n: ");
+            //int n = int.Parse(Console.ReadLine());
+
+            //Console.Write("m: ");
+            //int m = int.Parse(Console.ReadLine());
+            //int count = 0;
+
+            //for (int i = n; i <= m; i++)
+            //{
+            //    if (i % 2 == 1)
+            //    {
+            //        count++;
+            //    }
+            //}
+            //Console.WriteLine("say " +count);
+
+
+
+            //n ededinin 2-nin quvveti olub olmadini yoxlayin
+
+            //Console.Write("n: ");
+            //int n = int.Parse(Console.ReadLine());
+
+            //for (int i = n; n!=1; i++)
+            //{
+            //    if (n % 2 == 0)
+            //    {
+            //        n /= 2;
+            //    }
+            //    else
+            //    {
+            //        break;
+            //    }
+            //}
+
+            //if (n != 1)
+            //{
+            //    Console.WriteLine("Quvveti deyil");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Quvvetidir");
+            //}
+
+            //n ededinin faktorialini tapin
+
+            //Console.Write("n ededini daxil edin: ");
+            //int n = int.Parse(Console.ReadLine());
+            //int result = 1;
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    result *= i;
+
+            //}
+            //Console.WriteLine(result);
+
+        }
+    }
+}
